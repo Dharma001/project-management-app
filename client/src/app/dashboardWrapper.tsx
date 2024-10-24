2e33529e91923148ce385f204b43faa4e76604b4
Dharma001 <demonplays466@gmail.com>
2024-10-25 00:12:53 +0545

    feat: Added custome tailwindcss color code

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <div>dashboardWrapper</div>
+  )
+}
+
+export default DashboardWrapper
