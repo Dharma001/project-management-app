5294688a3bcf7124897634396df5c9aca91ccaf1
Dharma001 <demonplays466@gmail.com>
2024-10-25 18:09:56 +0545

    setting up the dashboard

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
+'use client'
+{/*folder file archi*/}
+import React, { useEffect } from 'react'
 import Navbar from "@/app/(components)/Navbar"
 import Sidebar from "@/app/(components)/Sidebar"
+import StoreProvider, { useAppDispatch, useAppSelector } from './redux'
+import { Icon, LucideIcon } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
-const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+const DashboardLayout = ({ children }: {children: React.ReactNode}) => {
+ const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+ const isDarkMode = useAppSelector((state) => state.global.isDarkMode)
+
+ useEffect(() => {
+  if(isDarkMode) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+ })
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-      
       <Sidebar />
-      <main className="flex flex-col w-full bg-gray-50 dark:bg-dark-bg md:pl-64">
+      <main className={`flex flex-col w-full bg-gray-50 dark:bg-dark-bg ${
+        isSidebarCollapsed ? '' : 'md:pl-64'}`}>
           <Navbar />
         {children}
       </main>
@@ -15,4 +31,12 @@ const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
   )
 }
 
-export default DashboardWrapper
+const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  )
+}
+
+export default DashboardWrapper;
