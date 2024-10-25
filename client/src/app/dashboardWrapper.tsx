19556d3f6c9f736f444e02e1ce8a0a83bd10ebba
Dharma001 <demonplays466@gmail.com>
2024-10-25 12:49:46 +0545

    fix: fixed the color code of tailwind config and added styles in dashboard

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from "@/app/(components)/Navbar"
+import Sidebar from "@/app/(components)/Sidebar"
 
 const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
       
-      Sidebar
+      <Sidebar />
       <main className="flex flex-col w-full bg-gray-50 dark:bg-dark-bg md:pl-64">
           <Navbar />
         {children}
