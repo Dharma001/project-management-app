bef300923cb7f4661234c174f9139ac0b4978b01
Dharma001 <demonplays466@gmail.com>
2024-10-27 23:28:08 +0545

    feat: added drag drop the tasks with showing tasks

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -5,6 +5,7 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 import { Task as TaskType } from '@/state/api'
 import { EllipsisVertical, Plus } from 'lucide-react';
 import { format } from 'date-fns';
+import Image from 'next/image';
 
 type BoardProps = {
   id: string;
@@ -107,9 +108,9 @@ const TaskColumn =({
     </div>
     {tasks
       .filter((task) => task.status === status)
-      .map((task) => {
-      <Task Key={task.id} task={task} />
-    })}
+      .map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
     </div>
   )
 }
@@ -119,7 +120,7 @@ type TaskProps = {
 }
 
 const Task = ({ task }: TaskProps) => {
-  const [{ isDragging} , drop] = useDrag(() => ({
+  const [{ isDragging} , drag] = useDrag(() => ({
     type: "task",
     item: {id: task.id},
     collect: (monitor: any) => ({
@@ -127,12 +128,87 @@ const Task = ({ task }: TaskProps) => {
     })
   }));
 
-  const taskTagsSplit = task.tags ? task.tags.split(",") : [],
+  const taskTagsSplit = task.tags ? task.tags.split(",") : [];
 
   const formattedStartDate = task.startDate ? format(new Date(task.startDate), "P") : '';
   const formattedDueDate = task.dueDate ? format(new Date(task.dueDate), "P") : '';
 
-  const numberOfComments = (task.comments && task.comments.length) || 0
+  const numberOfComments = (task.comments && task.comments.length) || 0;
 
+  const PriorityTag = ({ priority }: { priority: TaskType["priority"]}) => (
+    <div className={`rounded-full px-2 py-1 text-xs font-semibold ${
+      priority === "Urgent" 
+        ? 'bg-red-200 text-red-700' 
+        : priority === 'High'
+        ? 'bg-yellow-200 text-yellow-700' 
+        : priority === "Medium" 
+        ? 'bg-green-200 text-green-700' 
+        : priority === "Low" 
+        ? 'bg-blue-200 text-blue-700' 
+        : 'bg-gray-200 text-gray-700'
+      }`}>
+      {priority}
+    </div>
+  )
+
+  return (
+    <div
+      ref={(instance) => {
+        drag(instance)
+      }}
+      className={`mb-4 rounded-md bg-white shadow dark:bg-dark-secondary ${
+        isDragging ? 'opacity-50' : 'opacity-100'
+      }`}
+    >
+      {task.attachments && task.attachments.length > 0 && (
+        <Image
+          src={`/${task.attachments[0].fileURL}`}
+          alt={task.attachments[0].fileName}
+          width={400}
+          height={200}
+          className='h-auto w-full rounded-t-md '
+         />
+      )}
+      <div className='p-4 md:p-6'>
+        <div className='flex items-start justify-between'>
+          <div className='flex flex-1 flex-wrap items-center gap-2'>
+            {task.priority && 
+              <PriorityTag priority={task.priority}
+              />
+            }
+            <div className='flex gap-2'>
+              {taskTagsSplit.map((tag) => (
+                <div 
+                  key={tag}
+                  className='rounded-full bg-blue-100 px-2 py-1 text-xs'
+                >
+                  {' '}
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </div>
+          <button className='flex h-6 w-4 flex-shrink-0 items-center justify-center dark:text-neutral-500'>
+            <EllipsisVertical size={26} />
+          </button>
+        </div>
+        <div className='my-3 flex justify-between'>
+          <h4 className='text-md font-bold  dark:text-white'>
+            {task.title}
+          </h4>
+          {typeof task.points === "number" && (
+            <div className='text-xs font-semibold  dark:text-white'>
+              {task.points}
+            </div>
+          )}
+        </div>
+        <div className='text-xs text-gray-500 dark:text-neutral-500'>
+          {formattedStartDate && <span>{formattedStartDate} - </span>}
+          {formattedDueDate && <span>{formattedDueDate}</span>}
+        </div>
+        <p></p>
+      </div>
+    </div>
+  )
 }
 export default BoardView
